af1f436bdd497ec9e7ad40460aff2be1b2b77c93
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-03-15 23:11:31 +0800

    Add material design

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -24,23 +28,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun NewsStory(){
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ){
-        Image (
-            painter = painterResource(id = R.drawable.header),
-            contentDescription = null,
-            modifier = Modifier
-                .height(180.dp)
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
-        Spacer(Modifier.height(16.dp))
-        Text("A day in Shark Fin Cove")
-        Text("Davenport")
-        Text("December 2018")
+fun NewsStory() {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                "A quick brown fox jumps over the lazy dog.A quick brown fox jumps over the lazy dog.A quick brown fox jumps over the lazy dog.",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                "Davenport",
+                style = typography.body2
+            )
+            Text(
+                "December 2018",
+                style = typography.body2
+            )
+        }
     }
+
 }
 
 @Preview

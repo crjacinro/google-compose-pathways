5a158464e8db9dc28354345b3a82ecdccdc68e22
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-03-20 21:08:17 +0800

    Add "Declarative UI" codes

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,72 +20,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ComposeAppTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text("Hello $name")
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeAppTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
 }
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android, there")) {
+    Column {
+        names.forEach {
+            Greeting(it)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val padding = Modifier.padding(24.dp)
+    Text("Hello $name", modifier = padding)
+}
+
+
+
+
 
 @Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
-    ComposeAppTheme {
-        Greeting(name = "Android")
+    MyApp {
+        MyScreenContent()
     }
 }
-
-
-/*
-    Code from Jetpack Intro Article
- */
-
-//@Composable
-//fun NewsStory() {
-//    MaterialTheme {
-//        val typography = MaterialTheme.typography
-//        Column(
-//            modifier = Modifier.padding(16.dp)
-//        ) {
-//            Image(
-//                painter = painterResource(id = R.drawable.header),
-//                contentDescription = null,
-//                modifier = Modifier
-//                    .height(180.dp)
-//                    .fillMaxWidth()
-//                    .clip(shape = RoundedCornerShape(4.dp)),
-//                contentScale = ContentScale.Crop
-//            )
-//
-//            Spacer(Modifier.height(16.dp))
-//
-//            Text(
-//                "A quick brown fox jumps over the lazy dog.A quick brown fox jumps over the lazy dog.A quick brown fox jumps over the lazy dog.",
-//                style = typography.h6,
-//                maxLines = 2,
-//                overflow = TextOverflow.Ellipsis
-//            )
-//            Text(
-//                "Davenport",
-//                style = typography.body2
-//            )
-//            Text(
-//                "December 2018",
-//                style = typography.body2
-//            )
-//        }
-//    }
-//}
-//
-//@Preview
-//@Composable
-//fun DefaultPreview(){
-//    NewsStory()
-//}
